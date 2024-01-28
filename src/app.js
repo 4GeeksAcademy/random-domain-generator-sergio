08eb7df3cd7dc1dd9ca92164cfbf10eaf2c6f77f
SergioMontoro <84936651+SergioMontoro@users.noreply.github.com>
2024-01-28 13:47:47 +0000

    Random Domain Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  let domainPronoun = "";
+  let domainAdj = "";
+  let domainNoun = "";
+
+  for (let i = 0; i < 1; i++) {
+    for (let x = 0; x < pronoun.length; x++) {
+      domainPronoun = pronoun[x];
+      for (let y = 0; y < adj.length; y++) {
+        domainAdj = adj[y];
+        for (let z = 0; z < noun.length; z++) {
+          domainNoun = noun[z];
+
+          let domain = domainPronoun + domainAdj + domainNoun + ".com";
+          console.log(domain);
+        }
+      }
+    }
+  }
 };
